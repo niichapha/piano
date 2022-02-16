50622073ae832d15181d78f0b01ed99d2a82b5ec
Haris Alam <79714825+harisalam@users.noreply.github.com>
2022-02-16 12:20:37 -0600

    Remove duplication

--- rainbow_controller.py
+++ rainbow_controller.py
@@ -2,23 +2,10 @@
 import board
 import neopixel
 
-class RainbowController:
+import Controller
+
+class RainbowController(Controller):
     DOWN = 144
-
-    def __init__(self, num_lights):
-        # state of the lights
-        self.num_lights = num_lights
-        self.color_on = (255,255,255)
-        self.color_off = (0,0,0)
-        self.next_light = 0
-        self.prev_light = 0
-
-        # initialize the lights array, turn all lights off
-        self.pixels = neopixel.NeoPixel(board.D18,
-                                        self.num_lights,
-                                        brightness=1,
-                                        pixel_order=neopixel.RGB)
-        self.pixels.fill((0,0,0))
 
 
     def process_event(self, event):
