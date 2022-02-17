a25e6636bd0eed4f3195d3a4554024434bdcc98a
Kate Holdener <kate.holdener@slu.edu>
2022-02-17 15:19:19 -0600

    fixed indentation issues

--- lights_api/mock_api.py
+++ lights_api/mock_api.py
@@ -1,6 +1,6 @@
 class MockApi:
     def __init__(self, num_lights):
-        self.lights = list();
+        self.lights = []
         self.num_lights = num_lights
         self.set_all((0,0,0))
         print("created",num_lights,"lights")
@@ -11,10 +11,10 @@ class MockApi:
 
     def set_all(self, color):
         for i in range(0,self.num_lights):
-           if (i >= len(self.lights)):
-               self.lights.append(color)
-           else:
-               self.lights[i] = color
+            if (i >= len(self.lights)):
+                self.lights.append(color)
+            else:
+                self.lights[i] = color
 
     def get_num_lights(self):
         return self.num_lights
