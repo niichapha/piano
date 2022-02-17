770d607aec6447d78e8eab9dfe49e0f8bd777132
Kate Holdener <kate.holdener@slu.edu>
2022-02-17 12:17:10 -0600

    fixing pylint issues

--- light_controler/simple_controller.py
+++ light_controler/simple_controller.py
@@ -2,7 +2,7 @@ from .light_controller import LightController
 class SimpleController(LightController):
     DOWN = 144
     def __init__(self, num_lights, color_on, color_off):
-        self.__init__(num_lights, color_on, color_off)
+        LightController().__init__(num_lights, color_on, color_off)
 
     def process_event(self, event):
         message, deltatime = event
