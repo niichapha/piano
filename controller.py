9a505f9a481b406e7150750c86b3a15745f6fb3e
Haris Alam <79714825+harisalam@users.noreply.github.com>
2022-02-18 12:24:14 -0600

    Reomoved indentation erro

--- controller.py
+++ controller.py
@@ -2,18 +2,19 @@ import board
 import neopixel
 
 class Controller:
-     def __init__(self, num_lights, color_on, color_off):
-         DOWN = 144
-        # state of the lights
-         self.num_lights = num_lights
-         self.color_on = color_on
-         self.color_off = color_off
-         self.next_light = 0
-         self.prev_light = 0
+    def __init__(self, num_lights, color_on, color_off):
 
-        # initialize the lights array, turn all lights off
-         self.pixels = neopixel.NeoPixel(board.D18,
-                                        self.num_lights,
-                                        brightness=1,
-                                        pixel_order=neopixel.RGB)
-         self.pixels.fill((0,0,0))
+        DOWN = 144
+    # state of the lights
+        self.num_lights = num_lights
+        self.color_on = color_on
+        self.color_off = color_off
+        self.next_light = 0
+        self.prev_light = 0
+
+    # initialize the lights array, turn all lights off
+        self.pixels = neopixel.NeoPixel(board.D18,
+                                    self.num_lights,
+                                    brightness=1,
+                                    pixel_order=neopixel.RGB)
+        self.pixels.fill((0,0,0))
