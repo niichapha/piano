ce096112b7cb429caa33e747a02272cf17b8087f
Kate Holdener <kate.holdener@slu.edu>
2022-03-29 17:56:47 -0500

    restructured code to use clean architecture approach

--- main_simple.py
+++ main_simple.py
@@ -1,8 +1,19 @@
-from light_controller import simple_controller
+import sys
+import queue
+
+from light_controllers import simple_controller, rainbow_controller
 from lights_api import mock_api
-from piano_lights import LightsFromPiano
+from inputs.midi_input import MidiInput
+from use_cases.event_queue_runner import EventQueueRunner
 
 api = mock_api.MockApi(100)
-lights_controller = simple_controller.SimpleController(api, (244,0,0), (0,0,255))
-piano_input = LightsFromPiano()
-piano_input.run(lights_controller)
+
+if len(sys.argv) > 1 and sys.argv[1]=='rainbow':
+   lights_controller = rainbow_controller.RainbowController(api)
+else:
+   lights_controller = simple_controller.SimpleController(api, (244,0,0), (0,0,255))
+
+event_queue = queue.Queue(1024)
+midi_input = MidiInput(event_queue)
+runner = EventQueueRunner(event_queue, lights_controller)
+runner.run()
